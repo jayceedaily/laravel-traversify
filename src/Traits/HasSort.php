3340e7788ca5b20564d9bb146c1dd473fbf22fd6
Jaycee Mariano <jayceedaily@gmail.com>
2021-07-20 10:01:33 +0800

    refactor

--- src/Traits/HasSort.php
+++ src/Traits/HasSort.php
@@ -2,13 +2,15 @@
 namespace Traversify\Traits;
 
 use Exception;
-use Illuminate\Database\Eloquent\Builder;
 use RuntimeException;
 use InvalidArgumentException;
 use Kirschbaum\PowerJoins\PowerJoins;
+use Illuminate\Database\Eloquent\Builder;
 
 trait HasSort
 {
+    protected $hasSortRelationshipDriver = 'PowerJoins';
+
     use PowerJoins;
 
     /**
@@ -20,25 +22,29 @@ trait HasSort
      * @throws RuntimeException
      * @throws InvalidArgumentException
      */
-    public function scopeSort(Builder $query, Array $sort)
+    public function scopeSort(Builder $query, Array $sort = [])
     {
         if(!$this->sort) {
             throw new Exception("No column configured to be sorted");
         }
 
+        if(empty($sort)) {
+            return;
+        }
+
         foreach($this->sort as $sortable) {
 
-            if(in_array($sortable, array_keys($sort))) {
+            if (in_array($sortable, array_keys($sort))) {
 
                 $sortables = explode('.', $sortable);
 
                 $sortColumn = array_pop($sortables);
 
-                if(count($sortables) >= 1) {
+                if (count($sortables) >= 1) {
 
-                    $query->leftJoinRelationship(\implode('.', $sortables));
+                    $query->leftJoinRelationship(implode('.', $sortables));
 
-                    $relationshipTable = \array_pop($sortables);
+                    $relationshipTable = array_pop($sortables);
 
                     $tableName = $this->$relationshipTable()->getRelated()->getTable();
 
