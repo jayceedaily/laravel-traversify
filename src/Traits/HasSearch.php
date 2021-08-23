b8034f467b2534890cf6ec0403044364309cdef1
Jaycee Daily <jayceedaily@gmail.com>
2021-08-23 08:18:25 +0800

    refactor search

--- src/Traits/HasSearch.php
+++ src/Traits/HasSearch.php
@@ -4,6 +4,7 @@ namespace Traversify\Traits;
 use Exception;
 use RuntimeException;
 use InvalidArgumentException;
+use Illuminate\Support\Facades\Log;
 use Kirschbaum\PowerJoins\PowerJoins;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -24,7 +25,7 @@ trait HasSearch
      */
     public function scopeSearch(Builder $query, String $keyword = '')
     {
-        if (!$this->traversify || ($this->traversify && !$searches = $this->traversify['search'])) {
+        if (!$searches = $this->search) {
             throw new Exception('No column configured to be searched');
         }
 
@@ -39,23 +40,16 @@ trait HasSearch
             $this->like = 'ILIKE';
         }
 
-        $query->where(function($query) use($searches, $keyword){
+        // TODO The scope where query is cancelling powerjoi queries
+        // $query->where(function($query) use($searches, $keyword){
 
             foreach($searches as $searchable) {
 
-                if($this->hasSearchRelationshipDriver == 'PowerJoin') {
+                $this->createPowerJoinSearchQuery($query, $searchable, $keyword);
 
-                    $this->createPowerJoinSearchQuery($query, $searchable, $keyword);
-
-                } else {
-
-                    $searchables = explode('.', $searchable);
-
-                        $this->createEloquentSearchQuery($query, $searchables, $keyword );
-
-                }
             }
-        });
+        // });
+
     }
 
     private function createPowerJoinSearchQuery(Builder $query, String $searchable, String $keyword)
@@ -64,11 +58,13 @@ trait HasSearch
 
         $searchColumn = array_pop($searchables);
 
-        if (count($searchables) >= 1) {
+        if (count($searchables)) {
+
 
             $query->leftJoinRelationship(implode('.',$searchables));
 
             $relationshipTable = array_pop($searchables);
+
 
             $tableName = $this->$relationshipTable()->getRelated()->getTable();
 
@@ -77,6 +73,7 @@ trait HasSearch
             foreach ($keywords as $_keyword) {
 
                 $query->orWhere("$tableName.$searchColumn", $this->like, "%$_keyword%" );
+
             }
 
         } else {
@@ -91,76 +88,6 @@ trait HasSearch
             }
         }
 
-        return $query;
-    }
-
-    /**
-     * Generate search query using Eloquent relationships
-     *
-     * @param Builder $query
-     * @param array $searchable
-     * @param string $keyword
-     * @return void
-     * @throws RuntimeException
-     * @throws InvalidArgumentException
-     */
-    private function createEloquentSearchQuery(Builder $query, Array $searchable, String $keyword)
-    {
-        $searchColumn = array_shift($searchable);
-
-        if (count($searchable)) {
-
-            $query->orWhereHas($searchColumn, function($_query) use ($searchable, $keyword) {
-
-                $_query->where(function($_query) use ($searchable, $keyword) {
-
-                    $this->createEloquentSearchQuery($_query, $searchable, $keyword);
-                });
-            });
-
-        } else {
-
-            $query->orWhere(function($_query) use ($searchColumn, $keyword) {
-
-                $keywords = explode(" ", $keyword);
-
-                foreach ($keywords as $_keyword) {
-
-
-                    $_query->orWhere($searchColumn, $this->like, "%$_keyword%" );
-
-                }
-            });
-        }
-    }
-
-    /**
-     * Set searchable on runtime
-     *
-     * @param Illuminate\Database\Eloquent\Builder $query
-     * @param mixed $params
-     * @return void
-     */
-    public function scopeSetSearchable(Builder $query, Mixed $params)
-    {
-        if (!is_array($params)) {
-            $params = [$params];
-        }
-
-        $searchables = array_merge($this->searchable, $params);
-
-        $this->searchable = $searchables;
-    }
-
-    /**
-     * Set searchable on runtime
-     *
-     * @param Illuminate\Database\Eloquent\Builder $query
-     * @param mixed $params
-     * @return void
-     */
-    public function scopeResetSearchable(Builder $query, Mixed $params)
-    {
-        $this->searchable = [];
+        $query;
     }
 }
